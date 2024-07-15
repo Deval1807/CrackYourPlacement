dec7e804a844d1892c3676eca59de8e9c8824551
Deval <deval135darji@gmail.com>
2024-07-15 09:17:57 +0530

    Day 1: Find the Duplicate Number

---
+++ Day 1/find_the_duplicate_number.cpp
@@ -0,0 +1,20 @@
+/*
+Link: https://leetcode.com/problems/find-the-duplicate-number/description/
+Method: take ith number and considering it index, maked the indexth number negative. so the for duplicate no, it will already be negative
+Time Complexity: O(n)
+Space Complexity: o(1)
+*/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int i = 0; i<nums.size(); i++) {
+            int index = abs(nums[i]);
+            if(nums[index] < 0) {
+                return index;
+            }
+            nums[index] = -nums[index];
+        }
+        return -1;
+    }
+};
