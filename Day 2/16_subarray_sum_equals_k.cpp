8d4b9da2863f891096f05da9823c29c89d97f14c
Deval <deval135darji@gmail.com>
2024-07-16 11:11:47 +0530

    Day 2: Subarray Sum Equals K code

--- Day 2/16_subarray_sum_equals_k.cpp
+++ Day 2/16_subarray_sum_equals_k.cpp
@@ -4,3 +4,28 @@ Method: Prefix Sum
 Time Complexity: O(n)
 Space Complexity: o(n) extra
 */
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        // Prefix sum technique
+        // at a point if the Prefix Sum is S, and we want to know how many subarrays are there till
+        // that point with sum k
+        // we will find how many previously have sum S-k
+
+        int prefixSum = 0;
+        int ans = 0;
+        unordered_map<int,int> mp;
+        mp[0]=1;
+
+        for(int i = 0; i<nums.size(); i++) {
+            prefixSum+=nums[i];
+            if(mp.find(prefixSum-k) != mp.end()) {
+                ans+=mp[prefixSum-k];
+            }
+            mp[prefixSum]++;
+        }
+
+        return ans;
+    }
+};
