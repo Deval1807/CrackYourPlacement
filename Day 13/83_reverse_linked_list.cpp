bafe92a52487c4538397cfd56ea04828a6971157
Deval <deval135darji@gmail.com>
2024-07-27 22:58:18 +0530

    Day 13: Reverse Linked List

---
+++ Day 13/83_reverse_linked_list.cpp
@@ -0,0 +1,33 @@
+/*
+Link: https://leetcode.com/problems/reverse-linked-list/
+Method: 
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
