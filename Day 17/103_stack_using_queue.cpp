e966e579192ad75139fdfc67207f6e6a5d9e00af
Deval <deval135darji@gmail.com>
2024-07-31 21:34:48 +0530

    Day 17: Stack Using Queue

---
+++ Day 17/103_stack_using_queue.cpp
@@ -0,0 +1,45 @@
+/*
+Link: https://leetcode.com/problems/backspace-string-compare/description/
+Method: push into queue, and then remove all the ahead elements and push again them
+Time Complexity: O(n)
+Space Complexity: O(n) 
+*/
+
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i = 0; i<q.size()-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
